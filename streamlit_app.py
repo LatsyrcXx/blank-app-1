aab1e5a3879b882fabb63d13c2ef8cdc384da7b6
LatsyrcXx <crystalsoto70@gmail.com>
2026-04-10 03:44:36 +0000

    break_scheduler_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.write(
+st.write("🎈 My new Streamlit app")
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
